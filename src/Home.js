201c7b15bd4acde9fbf7fbf12ab7f46b973c341e
ThiagoYure <thigoyure@gmail.com>
2021-05-13 19:08:15 -0300

    adicionando pacotes de navegação e criando página Home

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class Home extends Component {
+    render() {
+        return(
+            <Text>Tela Inicial</Text>
+        );
+    }
+}
+
+export default Home;
