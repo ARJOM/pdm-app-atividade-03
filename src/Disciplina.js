8872740beafdcb15f674d22fa9b4a63b66038b88
Antônio Ricart <ricarteusuo11@gmail.com>
2021-05-14 09:24:58 -0300

    fix: corrige comunicacao com a api

--- src/Disciplina.js
+++ src/Disciplina.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import api from './services/api';
-import { getId, getToken } from './services/persistToken';
+import {Text, View} from 'react-native';
 
-const Disciplina = ({ navigation, route }) => {
+const Disciplina = ({navigation, route}) => {
   return (
     <View>
       <Text>Nome da Disciplina: {route.params.disciplina.name}</Text>
       <Text>Carga Horária: {route.params.disciplina.workload}</Text>
     </View>
   );
-
-}
+};
 
 export default Disciplina;
--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,45 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, Button, FlatList} from 'react-native';
 import api from './services/api';
-import { getId, getToken } from './services/persistToken';
+import {getId, getToken} from './services/persistToken';
 
-const Home = ({ navigation, route }) => {
+const Home = ({navigation, route}) => {
   const [discente, setDiscente] = useState({});
 
   useEffect(() => {
     getToken().then(res => {
       getId().then(r => {
         api
-          .get(`discentes/id=${r}`, { headers: { 'x-access-token': res } })
-          .then(result => setDiscente(result.data));
+          .get(`discentes/id=${r}`, {headers: {'x-access-token': res}})
+          .then(result => {
+            setDiscente(result.data);
+          });
       });
     });
   }, []);
   return (
     <View>
-      <Text>Bem vindo {discente.nome}!</Text>
-      <Text>Nome: {discente.nome}</Text>
+      <Text>Bem vindo {discente.name}!</Text>
+      <Text>Nome: {discente.name}</Text>
       <Text>Email: {discente.email}</Text>
       <Text>Disciplinas:</Text>
       <FlatList
-        data={[
-          discente.subjects
-        ]}
-        renderItem={
-          ({ item }) => {
-            <View>
-              <Text>{item.name}</Text>
-              <Button title="+info" onPress={() => navigation.navigate('Disciplina', { disciplina: item })}/>
-            </View>           
-          }
-        }
-      />
-      <Button
-        title="App"
-        onPress={() =>
-          navigation.navigate('App')
-        }
+        data={discente.subjects}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.name}</Text>
+            <Button
+              title="+info"
+              onPress={() =>
+                navigation.navigate('Disciplina', {disciplina: item})
+              }
+            />
+          </View>
+        )}
+        keyExtractor={item => item.id}
       />
     </View>
   );
-
-}
+};
 
 export default Home;
